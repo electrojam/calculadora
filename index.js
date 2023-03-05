2d755eba6f59ec1c9571ae7db56ffa1902adf859
electrojam <electrojam@hotmail.com>
2023-03-05 12:56:48 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+//Suma, reta, multiplicación, división
+
+function suma() {
+  resultado = 5 + 6
+  console.log("Suma 5 + 6 = "+resultado)
+}
+
+function resta() {
+  resultado = 5 - 6
+  console.log("Resta 5 - 6 = "+resultado)
+}
+
+function multiplicacion() {
+  resultado = 5 * 6
+  console.log("Multiplicación 5 * 6 = "+resultado)
+}
+
+function divicion() {
+  resultado = 5 / 6
+  console.log("División 5 / 6 = "+resultado)
+}
+
+suma()
+resta()
+multiplicacion()
+divicion()
